1082f2a3b49d4e1920a5923c898ba505976e4c60
ssfivy <2441149-ssfivy@users.noreply.gitlab.com>
2020-05-23 02:08:36 +1000

    Still unable to reproduce the bug on this machine

--- git/bug-reproduce-1/reproduce-bug.sh
+++ git/bug-reproduce-1/reproduce-bug.sh
@@ -8,6 +8,10 @@ set -e
 THIS_SCRIPT_DIR=$(dirname $(realpath -s $0))
 WORKDIR=$THIS_SCRIPT_DIR/lrn_work
 
+echo "Printing system information"
+cat /etc/os-release
+losetup -V
+
 echo "Cleaning work directory"
 rm -rf $WORKDIR
 
@@ -17,11 +21,31 @@ rm -rf $WORKDIR
 # or having to recompile git with custom $(prefix).
 echo "Setting up custom home directory"
 HOME=$WORKDIR/home/irving
+echo "Setting up mount point"
+MOUNT=$WORKDIR/media/irving/scratch
+mkdir -p $HOME $MOUNT
+
+echo "Creating file to be used as partition"
+dd if=/dev/zero of=partitionfile bs=1M count=100
+echo "Finding first unused loop device"
+LOOP_DEVICE=$(losetup -f)
+echo "Attaching image file to loop device $LOOP_DEVICE"
+sudo losetup "$LOOP_DEVICE" partitionfile
+echo "Partitioning the loop device with ext4"
+sudo mkfs.ext4 -c "$LOOP_DEVICE" 100000
+echo "Removing the loop device"
+sudo losetup -d "$LOOP_DEVICE"
+echo "Mounting the image file to $MOUNT"
+sudo mount -o loop partitionfile "$MOUNT"
+
+echo "Current mount config"
+mount | tail -n5
 
 echo "Setting up other directories"
-mkdir -p $HOME
-mkdir -p $WORKDIR/media/archives/src/github.com
-ln -s -T $WORKDIR/media/archives/src/           $HOME/src
+sudo mkdir -p $MOUNT/src
+sudo chown irving:irving $MOUNT/src
+mkdir -p $MOUNT/src/github.com
+ln -s -T $MOUNT/src/           $HOME/src
 
 echo "Setting up alternate git config files"
 cat << EOF > $HOME/.gitconfig
@@ -48,18 +72,22 @@ tree -a
 
 echo ""
 echo "Check out a sample repository"
-cd $WORKDIR/media/archives/src/github.com
+cd $MOUNT/src/github.com
 #cd $HOME/src/github.com
 git clone --depth 1 https://github.com/latchset/clevis.git
 
 echo ""
+cd $HOME/src/github.com/clevis
 echo "Current directory: $(pwd)"
 echo "Git configuration at repository root directory:"
-cd $HOME/src/github.com/clevis
 git config -l | cat
 
 echo ""
+cd $HOME/src/github.com/clevis/src
 echo "Current directory: $(pwd)"
 echo "Git configuration at repository subdirectory:"
-cd $HOME/src/github.com/clevis/src
 git config -l | cat
+
+echo "Cleanup"
+cd $WORKDIR
+sudo umount "$MOUNT"

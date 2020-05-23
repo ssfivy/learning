96f737a8f635dda29c61ea5b31f141cbe7a39e89
ssfivy <2441149-ssfivy@users.noreply.gitlab.com>
2020-05-23 18:39:16 +1000

    Killer sudoku solver works!

--- z3/sudokusolver/sudoku.py
+++ z3/sudokusolver/sudoku.py
@@ -9,49 +9,46 @@ import z3
 X = [[z3.Int("x_%s_%s" % (i+1, j+1)) for j in range(9)]
      for i in range(9)]
 pprint.pprint(X)
-print('-------------------')
+# print('-------------------')
 
 # each cell contains a value in {1, ..., 9}
 cells_c = [z3.And(1 <= X[i][j], X[i][j] <= 9)
            for i in range(9) for j in range(9)]
 # pprint.pprint(cells_c)
-print('-------------------')
+# print('-------------------')
 
 # each row contains a digit at most once
 rows_c = [z3.Distinct([X[i][j] for j in range(9)])
           for i in range(9)]
 # pprint.pprint(rows_c)
-print('-------------------')
+# print('-------------------')
 
 # each column contains a digit at most once
 cols_c = [z3.Distinct([X[i][j] for i in range(9)])
           for j in range(9)]
 # pprint.pprint(cols_c)
-print('-------------------')
+# print('-------------------')
 
 # each 3x3 square contains a digit at most once
 sq_c = [z3.Distinct([X[3*i0 + i][3*j0 + j]
                      for i in range(3) for j in range(3)])
         for i0 in range(3) for j0 in range(3)]
 # pprint.pprint(sq_c)
-print('-------------------')
+# print('-------------------')
 
 sudoku_c = cells_c + rows_c + cols_c + sq_c
 
 ###########################
 
 
-def gen_killer_cage(cage_coords, cage_sum):
+def add_killer_cage(s, cage_coords, cage_sum):
     '''Add a single killer sudoku cage.
     Takes a list of (row, col) coordinates and the sum
     '''
-    c = []
     cagecells = [X[i][j] for i, j in cage_coords]
     # Digits dont repeat within cages
-    c.append(z3.Distinct(cagecells))
-    c.append(z3.Sum([cagecells]) == cage_sum)
-    pprint.pprint(c)
-    return c
+    s.add(z3.Distinct(cagecells))
+    s.add(z3.Sum(cagecells) == cage_sum)
 
 # Try implementing solver for this: https://www.youtube.com/watch?v=QRM4T8dq9oc
 
@@ -67,18 +64,21 @@ cages.append({'coords': [(1, 7), (2, 7)], 'sum': 13})
 cages.append({'coords': [(2, 2), (2, 3), (3, 3)], 'sum': 20})
 cages.append({'coords': [(2, 4), (3, 4)], 'sum': 12})
 cages.append({'coords': [(2, 5), (3, 5), (2, 6)], 'sum': 12})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
-cages.append({'coords': [(,), (,), (,), (,), (,), ], 'sum': x})
+cages.append({'coords': [(3, 1), (3, 2), (4, 1), (5, 1), (5, 2), ], 'sum': 27})
+cages.append({'coords': [(3, 6), (3, 7), (4, 7), (5, 7), (5, 6), ], 'sum': 27})
+cages.append({'coords': [(4, 2), (4, 3)], 'sum': 9})
+cages.append({'coords': [(4, 5), (4, 6)], 'sum': 10})
+cages.append({'coords': [(5, 0), (6, 0), (7, 0), (8, 0), (8, 1), ], 'sum': 20})
+cages.append({'coords': [(5, 3), (6, 3), (6, 2)], 'sum': 8})
+cages.append({'coords': [(5, 4), (6, 4), ], 'sum': 16})
+cages.append({'coords': [(5, 5), (6, 5), (6, 6), ], 'sum': 14})
+cages.append({'coords': [(5, 8), (6, 8), (7, 8), (8, 8), (8, 7), ], 'sum': 19})
+cages.append({'coords': [(6, 1), (7, 1), ], 'sum': 11})
+cages.append({'coords': [(6, 7), (7, 7), ], 'sum': 13})
+cages.append({'coords': [(7, 2), (7, 3), (7, 4), (7, 5), (7, 6), ], 'sum': 23})
+cages.append({'coords': [(8, 2), (8, 3)], 'sum': 13})
+cages.append({'coords': [(8, 5), (8, 6)], 'sum': 13})
 
-
-cage_c = [gen_killer_cage(c['coords'], c['sum']) for c in cages]
 
 ###########################
 
@@ -109,6 +109,10 @@ instance_c = [z3.If(instance[i][j] == 0,
               for i in range(9) for j in range(9)]
 
 s = z3.Solver()
+
+for c in cages:
+    add_killer_cage(s, c['coords'], c['sum'])
+
 s.add(sudoku_c + instance_c)
 if s.check() == z3.sat:
     m = s.model()

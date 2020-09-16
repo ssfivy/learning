8c11cd1cc0cc265781114eabce182205ac3854ba
ssfivy <2441149-ssfivy@users.noreply.gitlab.com>
2020-09-16 17:12:55 +1000

    Fixed pathing to allow multiple upstreams

--- yocto/imx7-sabre/build.sh
+++ yocto/imx7-sabre/build.sh
@@ -8,15 +8,18 @@ UPSTREAM="upstream"
 
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 COCKATOODIR="$(realpath "$THISDIR/../../../")"
-BUILDDIR="$COCKATOODIR/build/conf"
+PROJECTNAME="$(basename "$THISDIR")"
+BUILDDIR="$COCKATOODIR/build/${UPSTREAM}/${PROJECTNAME}/conf"
 
 mkdir -p "$BUILDDIR"
 pushd "$THISDIR/conf/"
 cp  local.conf bblayers.conf "$BUILDDIR"
 popd
 
-source "$COCKATOODIR/$UPSTREAM/poky/oe-init-build-env" "$COCKATOODIR/build/$UPSTREAM"
+# shellcheck disable=1090
+source "$COCKATOODIR/$UPSTREAM/poky/oe-init-build-env" "$COCKATOODIR/build/$UPSTREAM/$PROJECTNAME"
 
+# print bunch of debugging stuff including who set what variables
 #bitbake -e
 
 bitbake -k core-image-full-cmdline

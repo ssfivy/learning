342af9be788e00c4c97e8a7d1c70aa2d8b8ee320
ssfivy <2441149-ssfivy@users.noreply.gitlab.com>
2020-10-23 10:50:46 +1100

    Create basic browser image

---
+++ yocto/imx7-browser/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Assumes we already are checked out by repotool
+
+set -e
+
+UPSTREAM="upstream2"
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+COCKATOODIR="$(realpath "$THISDIR/../../../")"
+PROJECTNAME="$(basename "$THISDIR")"
+BUILDDIR="$COCKATOODIR/build/${UPSTREAM}/${PROJECTNAME}/conf"
+
+mkdir -p "$BUILDDIR"
+pushd "$THISDIR/conf/"
+cp  local.conf bblayers.conf "$BUILDDIR"
+popd
+
+# shellcheck disable=1090
+source "$COCKATOODIR/$UPSTREAM/poky/oe-init-build-env" "$COCKATOODIR/build/$UPSTREAM/$PROJECTNAME"
+
+# print bunch of debugging stuff including who set what variables
+#bitbake -e
+
+bitbake -k core-image-weston

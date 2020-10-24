0a0db8cbcd7ba39ba4835a7047fec488134bea41
ssfivy <2441149-ssfivy@users.noreply.gitlab.com>
2020-10-24 15:16:25 +1100

    Add yocto browser recipe to stm32mp1

---
+++ yocto/stm32mp1-browser/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -xe
+
+UPSTREAM="upstream"
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+COCKATOODIR="$(realpath "$THISDIR/../../../")"
+PROJECTNAME="$(basename "$THISDIR")"
+BUILDDIR="$COCKATOODIR/build/${UPSTREAM}/${PROJECTNAME}/conf"
+
+mkdir -p "$BUILDDIR"
+pushd "$THISDIR/conf/"
+cp  local.conf bblayers.conf "$BUILDDIR"
+popd
+
+# shellcheck disable=1090
+source "$COCKATOODIR/$UPSTREAM/poky/oe-init-build-env" "$COCKATOODIR/build/$UPSTREAM/$PROJECTNAME"
+
+# print bunch of debugging stuff including who set what variables
+#bitbake -e
+
+bitbake -k core-image-sato
